be5669d9f2a32f216632780c2e1a85f9dc656828
Tifany <151220180+3st3fanyaOM@users.noreply.github.com>
2025-02-23 13:09:40 +0100

    23-02

--- pagina-charlie/js/script.js
+++ pagina-charlie/js/script.js
@@ -1,6 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const lazyImages = document.querySelectorAll(".carousel-item img");
-  lazyImages.forEach((img) => {
-    img.setAttribute("loading", "lazy");
-  });
+$(function () {
+  $(".twentytwenty-container").twentytwenty();
 });
